a54c6a03414f9a637c68a307594bf761778a738b
somshubhraroy55-a11y <somshubhraroy55@gmail.com>
2026-06-03 21:49:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
+void clearPicture() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            picture[i][j] = EMPTY;
+        }
+    }
+}
 
+void displayPicture() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            printf("%c", picture[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void setPixel(int x, int y) {
+    if(x >= 0 && x < WIDTH &&
+       y >= 0 && y < HEIGHT) {
+        picture[y][x] = PIXEL;
+    }
+}
