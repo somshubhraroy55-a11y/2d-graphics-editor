b253369c9422659f48bb3490bbd1c57f07f344fa
somshubhraroy55-a11y <somshubhraroy55@gmail.com>
2026-06-03 21:52:44 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WIDTH 80
+#define HEIGHT 24
+
+#define EMPTY '_'
+#define PIXEL '*'
+
+char picture[HEIGHT][WIDTH];
 void clearPicture() {
     for(int i = 0; i < HEIGHT; i++) {
         for(int j = 0; j < WIDTH; j++) {
