e6a94b522885487eaa5b573fb26c08226ad57ac6
somshubhraroy55-a11y <somshubhraroy55@gmail.com>
2026-06-04 19:56:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -31,3 +31,29 @@ void setPixel(int x, int y) {
         picture[y][x] = PIXEL;
     }
 }
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    int steps;
+
+    if(dx > dy)
+        steps = dx;
+    else
+        steps = dy;
+
+    float xIncrement = (float)(x2 - x1) / steps;
+    float yIncrement = (float)(y2 - y1) / steps;
+
+    float x = x1;
+    float y = y1;
+
+    for(int i = 0; i <= steps; i++)
+    {
+        setPixel((int)(x + 0.5), (int)(y + 0.5));
+
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
